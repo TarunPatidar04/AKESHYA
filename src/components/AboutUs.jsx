b181ab2da9ec349714159cd49d5bcdd7e023d3e2
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-09 11:55:36 +0530

    WIP working on AboutUS Component

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "remixicon/fonts/remixicon.css";
-
+import count from "../assets/count.svg";
+import smile from "../Icons/smile.png";
+import clock from "../Icons/clock.png";
+import world from "../Icons/world.png";
+import diary from "../Icons/diary.png";
 const AboutUs = () => {
   return (
     <div className="py-10  max-w-7xl mx-auto ">
       <div className="px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-gray-800 text-center text-black">
-          <span className="text-customBlue">&#9135;</span>ABOUT US
-          <span className="text-customBlue">&#9135;</span>{" "}
+        <h2 className="text-3xl font-bold text-center text-black">
+          <span className="text-customBlue">&#8212;</span>ABOUT US
+          <span className="text-customBlue">&#8212;</span>
         </h2>
         <div className="flex mt-10 w-[100%] text-[#444444]">
           <div className="space-y-4  w-[50%]">
@@ -51,6 +55,69 @@ const AboutUs = () => {
             <button className="mt-12 rounded-3xl py-2 px-8 border border-customBlue hover:text-white hover:bg-customBlue cursor-pointer">
               Learn More
             </button>
+          </div>
+        </div>
+        <div className="w-[100%] mt-12 flex">
+          <div className="w-[45%]">
+            <img src={count} alt="this is Icons" />
+          </div>
+          <div className="w-[55%] mt-16 ml-5">
+            <div className="">
+              <div className=" flex space-x-28">
+                <div className="flex space-x-2">
+                  <img
+                    src={smile}
+                    alt="this is Icons"
+                    className="w-9 h-9 filter  text-customBlue"
+                  />
+                  <p className="text-4xl font-bold font-sans -mt-1 ">
+                    3835039
+                    <p className="text-sm text-[#484848] mt-3">
+                      Organic Reach (Global)
+                    </p>
+                  </p>
+                </div>
+                <div className="flex space-x-2">
+                  <img
+                    src={diary}
+                    alt="this is Icons"
+                    className="w-9 h-9 filter  text-customBlue"
+                  />
+                  <p className="text-4xl font-bold font-sans -mt-1 ">
+                    85
+                    <p className="text-sm text-[#484848] mt-3">Campaigns</p>
+                  </p>
+                </div>
+              </div>
+              <div className="flex space-x-36 mt-10">
+                <div className="flex space-x-2">
+                  <img
+                    src={clock}
+                    alt="this is Icons"
+                    className="w-9 h-9 filter  text-customBlue"
+                  />
+                  <p className="text-4xl font-bold font-sans -mt-1 ">
+                    14081
+                    <p className="text-sm text-[#484848] mt-3">
+                      Watch Hours (Asia)
+                    </p>
+                  </p>
+                </div>
+                <div className="flex space-x-2">
+                  <img
+                    src={world}
+                    alt="this is Icons"
+                    className="w-9 h-9 filter  text-customBlue"
+                  />
+                  <p className="text-4xl font-bold font-sans -mt-1 ">
+                    17
+                    <p className="text-sm text-[#484848] mt-3">
+                      Excellent CTR % (Asia)
+                    </p>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
