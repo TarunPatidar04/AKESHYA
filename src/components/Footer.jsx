2f3cb8eedf04b9f9a0bd502b5e68648d03c18a67
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-10 13:33:05 +0530

    WIP fix the changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="flex justify-between px-6 lg:px-28 py-5 items-center">
+    <footer className="shadow-2xl shadow-black  px-6 ">
+      <div className="flex justify-between  lg:px-28 py-7 items-center shadow-lg backdrop-blur  bg-white">
         <div className="flex items-center">
-          <span className="ml-2 mt-0  text-[#222222] ">
+          <span className="ml-2 mt-0 text-[#222222]">
             © Copyright Akeshya. All Rights Reserved
           </span>
         </div>
         <div className="flex items-center">
           <ul className="flex items-center space-x-4 lg:space-x-6">
-            <li className="text-customBlue  cursor-pointer text-sm">
+            <li className="text-customBlue cursor-pointer text-sm">
               Terms and conditions
             </li>
-            <li className=" cursor-pointer text-customBlue text-sm">Refund policy</li>
-            <li className=" cursor-pointer text-customBlue text-sm">Privacy policy</li>
+            <li className="cursor-pointer text-customBlue text-sm">Refund policy</li>
+            <li className="cursor-pointer text-customBlue text-sm">Privacy policy</li>
           </ul>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="navbar fixed top-0 w-full bg-white z-10 ">
+    <header
+      className={`navbar fixed top-0 w-full bg-white z-10 transition-all duration-300 ${
+        isScrolled ? "shadow-lg shadow-[#cbcbcb] backdrop-blur-sm" : "shadow-none"
+      }`}
+    >
       <nav className="flex justify-between px-6 lg:px-28 py-5 items-center">
         <div className="flex items-center">
           <img src={Logo} alt="Company Logo" className="w-16" />
@@ -13,11 +36,9 @@ const Navbar = () => {
         </div>
         <div className="flex items-center">
           <ul className="flex items-center space-x-4 lg:space-x-6">
-            <li className="text-customBlue  cursor-pointer">
-              Home
-            </li>
-            <li className=" cursor-pointer">About</li>
-            <li className=" cursor-pointer">Services</li>
+            <li className="text-customBlue cursor-pointer">Home</li>
+            <li className="cursor-pointer">About</li>
+            <li className="cursor-pointer">Services</li>
             <li>
               <button className="bg-customBlue hover:bg-[#7889f6] text-white py-2 px-4 lg:px-6 rounded-3xl">
                 Contact us
